8cc875807fd66526bbe8048f4b2bcb8f85796dd3
Joseph <joseph.chn@outlook.com>
2025-01-16 17:29:27 +0800

    feat(sysdrv): 添加wifi模组sv6158驱动

---
+++ sysdrv/drv_ko/wifi/L.SWB.20Q2.2220.01_3314/include/ssv_firmware_version.h
@@ -0,0 +1,12 @@
+#ifndef _SSV_FRIMWARE_VERSION_H_
+#define _SSV_FRIMWARE_VERSION_H_
+
+static u32 ssv_firmware_version = 11951;
+
+#define SSV_FIRMWARE_URl "http://10.10.20.22/svn/wifi/firmware/branch/customer/Hualai6158BLE/WSDK.SWB.20Q2.2220.01"
+#define FRIMWARE_COMPILERHOST "twbcsrv1"
+#define FRIMWARE_COMPILERDATE "2023-09-22"
+#define FRIMWARE_COMPILEROS "linux"
+#define FRIMWARE_COMPILEROSARCH "x86_64-linux-gnu-thread-multi"
+
+#endif
